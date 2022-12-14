494eefab5073e1ac768d42b3c5cd8e6e25f9813e
CalixtoFull <120605885+CalixtoFull@users.noreply.github.com>
2022-12-14 19:49:22 -0300

    Add files via upload

---
+++ APPFULL/src/js/index.js
@@ -0,0 +1,15 @@
+function openPage(pageName) {
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+
+  document.getElementById(pageName).style.display = "block";
+}
+
+document.getElementById("defaultOpen").click();
